f5ea420e4ec3f961fae122647b528c8171f530ba
Dima Frolenko <mail@frolenkodima.ru>
2023-11-13 04:35:03 +0300

    feat: storage добавить пакет помощник для обработки запросов scan

---
+++ internal/storage/scan/scanner.go
@@ -0,0 +1,33 @@
+// Пакет помощник для создания струкур после запросов селект
+//
+// Пример:
+//
+//	for rows.Next(){
+//		 entity=scan.Subscription(rows)
+//	}
+package scan
+
+import "gitlab.com/thefrol/notty/internal/entity"
+
+type Scanner interface {
+	Scan(...any) error
+}
+
+func Subscription(r Scanner) (entity.Subscription, error) {
+	res := entity.Subscription{}
+
+	err := r.Scan(
+		&res.Id,
+		&res.Text,
+		&res.Start,
+		&res.End,
+		&res.OperatorFilter,
+		&res.PhoneFilter,
+		&res.TagFilter,
+		&res.Desc)
+	if err != nil {
+		return entity.Subscription{}, err
+	}
+
+	return res, nil
+}
