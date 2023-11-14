b83c9007b4d6e21e710625521094aa106e59cb89
Dima Frolenko <mail@frolenkodima.ru>
2023-11-14 09:49:41 +0300

    feat: Добавить приложение worker

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -12,13 +12,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 	"time"
 
-	"gitlab.com/thefrol/notty/internal/app"
-	"gitlab.com/thefrol/notty/internal/storage/messages"
+	"gitlab.com/thefrol/notty/internal/entity"
+	"gitlab.com/thefrol/notty/internal/postman"
 	"gitlab.com/thefrol/notty/internal/storage/postgres"
 	"gitlab.com/thefrol/notty/internal/storage/subscriptions"
+	"gitlab.com/thefrol/notty/internal/stream"
 )
+
+const batchSize = 50
+const timeout = 3 * time.Second
+const WorkerCount = 50
 
 func main() {
 	// конфигурируем
@@ -31,12 +37,97 @@ func main() {
 	// соединяемся с БД
 	db := postgres.MustConnect(dsn)
 
-	o := app.Obs{
-		MessagesRepo:      messages.New(db),
-		SubscriptionsRepo: subscriptions.New(db),
+	//создаем сервисы
+	mes := stream.Messages(db)
+
+	subs := subscriptions.New(db)
+
+	post := postman.Poster{}
+
+	//и за дело
+
+	for {
+		time.Sleep(timeout)
+
+		// проверяем сколько рассылок активно
+		active, err := subs.Active()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(active) == 0 {
+			// если никого, то спим
+			continue
+		}
+
+		// если же есть, то плодим горутины
+		// у нас работает конвеер
+		//
+		// Некоторые этапы можно посерьезней распараллелить,
+		// вопрос просто в желании, мне кажется параллелить
+		// надо только отправку
+
+		protos, err := mes.Generate(batchSize)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		msgs, err := mes.Create(protos)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// отправляем в куче горутин
+		var dones []chan entity.Message
+		for i := 0; i < WorkerCount; i++ {
+			done, err := post.Work(msgs)
+			if err != nil {
+				log.Fatal(err)
+			}
+			dones = append(dones, done)
+		}
+
+		/// собираем все в кучу и обновляем
+		end, err := mes.Update(FanIn(dones...))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		terminate(end)
+
 	}
 
-	o.FetchAndWork(3*time.Second, 2)
+}
 
-	log.Println("all done")
+func FanIn(ins ...chan entity.Message) chan entity.Message {
+	out := make(chan entity.Message)
+
+	wg := sync.WaitGroup{}
+	for _, in := range ins {
+		wg.Add(1)
+
+		in := in
+		go func() {
+			defer wg.Done()
+
+			for m := range in {
+				out <- m
+			}
+		}()
+	}
+	go func() {
+		wg.Wait()
+
+		fmt.Println("каналы закрыты")
+		close(out)
+
+	}()
+
+	return out
+}
+
+func terminate(ins ...chan entity.Message) {
+	end := FanIn(ins...)
+	for _ = range end {
+	}
 }
