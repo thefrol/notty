9a5f858cecb065edd87b85aefa3bbd16893fc7f5
Dima Frolenko <mail@frolenkodima.ru>
2023-11-14 10:48:13 +0300

    feat: Добавить сущности сообщений логику статусов

--- internal/entity/messages.go
+++ internal/entity/messages.go
@@ -4,12 +4,20 @@ import "time"
 
 // небольшая бизнес-логика сообщений
 const (
-	StatusSent   = "done"
-	StatusFailed = "fail"
+	StatusSent    = "done"
+	StatusFailed  = "fail"
+	StatusInvalid = "invalid"
 )
 
 // SentNow помечает, что сообщение было отправлено вот только что
 func (m *Message) SentNow() {
+	// todo
+	//
+	// тут надо подумать, а что если я
+	// я попытаюсь поставить там
+	// для неправильного сообщения что оно
+	// отправлено. возможно ли такое?
+	// итд
 	m.Status = StatusSent
 	t := time.Now()
 	m.Sent = &t
@@ -18,4 +26,9 @@ func (m *Message) SentNow() {
 // Failed помечает, что сообщение не удалось отправить
 func (m *Message) Failed() {
 	m.Status = StatusFailed
+}
+
+// Invalid помечает, что сообщение не удалось отправить
+func (m *Message) Invalid() {
+	m.Status = StatusInvalid
 }
